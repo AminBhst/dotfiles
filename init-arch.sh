911b15376932241d4b2c5127a4507a0133a83369
AminBhst <amin.bhst@gmail.com>
2025-05-20 16:26:26 +0330

    Add init-arch.sh

---
+++ init-arch.sh
@@ -0,0 +1,78 @@
+echo "Have you set up your SSH key for GitHub? (yes/no)"
+read -r answer
+
+if [[ "${answer,,}" != "yes" && "${answer,,}" != "y" ]]; then
+    echo "Please set up your SSH key for GitHub before proceeding."
+    exit 1
+fi
+
+cd ~
+
+# Fonts
+sudo pacman -S --noconfirm \
+  noto-fonts \
+  oto-fonts-cjk \
+  noto-fonts-emoji \
+  ttf-dejavu \
+  ttf-liberation \
+  ttf-farsiweb \
+  fonts-meta-base \
+  noto-fonts \
+  noto-fonts-extra \
+  noto-fonts-emoji
+
+
+# Packages
+sudo pacman -S --noconfirm \
+  git \
+  lazygit \
+  stow \
+  fzf \
+  dunst \
+  clang \
+  discord \
+  ninja-build \
+  libkeybinder3 \
+  libappindicator-gtk3 \
+  cmake \
+  waybar \
+  picom \
+  flatpak \
+  alacritty \
+  feh \
+  zsh \
+  neovim \
+  xprop \
+  NetworkManager-tui \
+  network-manager-applet \
+  telegram-desktop \
+  base-devel \
+  htop \
+  btop \
+  unrar \
+  vlc \
+  zoxide \
+  ripgrep \
+  polkit-gnome \
+  ffmpeg \
+  pavucontrol \
+  shutter \
+
+#  Download jetbrains font
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/JetBrainsMonoNLNerdFont-Regular.ttf
+
+# Setup configs
+git clone git@github.com:AminBhst/dotfiles.git ~/.dotfiles
+stow -d ~/.dotfiles -t ~ */
+
+# Setup nvim
+git clone git@github.com:AminBhst/nvim.git ~/.config/nvim
+  
+# Setup zsh
+sudo wget -P /usr/share/fzf/shell https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+sudo wget -P /usr/share/fzf/shell https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+chsh -s $(which zsh)
+
+# yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git ~/.yay-bin && cd ~/.yay-bin && makepkg -si
+yay
